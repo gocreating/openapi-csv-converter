779a94244c43fe50f9f89f9896b23a219fc82b96
Chih Ping Weng <chihping.weng@TW168020087M.local>
2020-05-28 10:25:10 +0800

    Fix additional properties for csv2oas

--- csv2oas.py
+++ csv2oas.py
@@ -120,9 +120,11 @@ def property_id_to_spec(property_id):
                 property_spec['properties'][partial_property_name] = property_id_to_spec(partial_property_id)
             if len(required_properties) > 0:
                 property_spec['required'] = required_properties
-            additional_properties = oas_property.get('object_additional_properties')
-            if additional_properties != None:
-                property_spec['additionalProperties'] = str2bool(additional_properties)
+        if oas_property.get('property_name') == 'extra_data':
+            print('pause')
+        additional_properties = oas_property.get('object_additional_properties')
+        if additional_properties != None:
+            property_spec['additionalProperties'] = str2bool(additional_properties)
         return { **property_spec, **validation_extras }
 
     elif oas_property_type == 'oneOf':
