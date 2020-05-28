63b548f004c0aa5fbd1d735521ef2eaa1ace4b2a
Chih Ping Weng <chihping.weng@TW168020087M.local>
2020-05-28 14:01:55 +0800

    Refine test

--- test_converter.py
+++ test_converter.py
@@ -1,26 +1,43 @@
 import os
 import yaml
 import unittest
+import jsonschema
 
-class TestConverter(unittest.TestCase):
+class ConverterTest(unittest.TestCase):
     maxDiff = None
     S1 = os.environ.get('S1')
     S2 = os.environ.get('S2')
 
-    def test_converter(self):
+    def __init__(self, *args, **kwargs):
+        super(ConverterTest, self).__init__(*args, **kwargs)
         with open(self.S1) as yaml_file:
-            raw_spec = yaml.load(yaml_file, Loader=yaml.FullLoader)
+            self.raw_spec = yaml.load(yaml_file, Loader=yaml.FullLoader)
         with open(self.S2) as yaml_file:
-            converted_spec = yaml.load(yaml_file, Loader=yaml.FullLoader)
+            self.converted_spec = yaml.load(yaml_file, Loader=yaml.FullLoader)
 
+    def test_json_schema(self):
+        # Assert both source schema and converted schema are valid jsonschema format
+        for schema_name, schema_spec in self.raw_spec['components']['schemas'].items():
+            jsonschema.Draft7Validator.check_schema(schema_spec)
+            jsonschema.Draft7Validator.check_schema(self.converted_spec['components']['schemas'][schema_name])
+
+        for path_name, path in self.raw_spec['paths'].items():
+            for operation_name, operation in self.raw_spec['paths'][path_name].items():
+                raw_schema = operation.get('requestBody', {}).get('content', {}).get('application/json', {}).get('schema')
+                converted_schema = self.converted_spec['paths'][path_name][operation_name].get('requestBody', {}).get('content', {}).get('application/json', {}).get('schema')
+                if raw_schema:
+                    jsonschema.Draft7Validator.check_schema(raw_schema)
+                    jsonschema.Draft7Validator.check_schema(converted_schema)
+
+    def test_converter(self):
         # Asset components.schemas
-        for schema_name, schema_spec in raw_spec['components']['schemas'].items():
-            self.assertDictEqual(schema_spec, converted_spec['components']['schemas'][schema_name])
+        for schema_name, schema_spec in self.raw_spec['components']['schemas'].items():
+            self.assertDictEqual(schema_spec, self.converted_spec['components']['schemas'][schema_name])
 
         # Assert `requestBody` only (responses are not supported yet)
-        for path_name, path in raw_spec['paths'].items():
-            for operation_name, operation in raw_spec['paths'][path_name].items():
+        for path_name, path in self.raw_spec['paths'].items():
+            for operation_name, operation in self.raw_spec['paths'][path_name].items():
                 self.assertDictEqual(
-                    raw_spec['paths'][path_name][operation_name]['requestBody'],
-                    converted_spec['paths'][path_name][operation_name]['requestBody']
+                    self.raw_spec['paths'][path_name][operation_name]['requestBody'],
+                    self.converted_spec['paths'][path_name][operation_name]['requestBody']
                 )
